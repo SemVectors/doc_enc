9330e5d9a77a8fcbe694a4480024c5300f8380f8
Denis Zubarev <dvzubarev@yandex.ru>
2022-04-14 15:21:30 +0300

    add combining tool

---
+++ doc_enc/training/combine_docs_sources.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+
+from typing import NamedTuple
+from pathlib import Path
+import csv
+import hashlib
+
+
+class Example(NamedTuple):
+    dataset: int
+    src_id: int
+    tgt_id: int
+    src_len: int
+    tgt_len: int
+    label: int
+    src_hash: str
+    tgt_hash: str
+
+
+def combine_docs_datasets(
+    input_dir,
+    split,
+    include_datasets=None,
+    exclude_datasets=None,
+    out_filename_prefix="combined",
+):
+    input_path = Path(input_dir)
+    datasets = []
+    for p in input_path.iterdir():
+        if not p.is_dir():
+            continue
+
+        entry = p.name
+        if exclude_datasets and entry in exclude_datasets:
+            continue
+        if include_datasets and entry not in include_datasets:
+            continue
+        datasets.append(p)
+
+    with open(input_path / f"{out_filename_prefix}_{split}.csv", 'w', encoding="utf-8") as outf:
+        csv_writer = csv.writer(outf)
+        csv_writer.writerow(("ds", "src", "tgt", "label", "slen", "tlen", "shash", "thash"))
+        if not datasets:
+            return
+
+        all_examples = []
+        for i, dsp in enumerate(datasets):
+            info_dict = _calc_sentence_size_and_hash(dsp)
+            all_examples.extend(_generate_examples_from_dataset(dsp, split, i, info_dict))
+
+        all_examples.sort(key=lambda t: (t.src_len, t.src_hash, -t.label, t.tgt_len))
+        for e in all_examples:
+            csv_writer.writerow(
+                (
+                    datasets[e.dataset].name,
+                    e.src_id,
+                    e.tgt_id,
+                    e.label,
+                    e.src_len,
+                    e.tgt_len,
+                    e.src_hash,
+                    e.tgt_hash,
+                )
+            )
+
+
+def _calc_sentence_size_and_hash(dataset_path: Path):
+    docs_path = dataset_path / "texts"
+    if not docs_path.exists():
+        raise RuntimeError(f"Not found texts folder in {dataset_path}")
+
+    info_dict = {}
+    for p in docs_path.iterdir():
+        if not p.is_file() or not p.name.endswith(".txt"):
+            continue
+        doc_id = int(p.name[:-4])
+        if doc_id in info_dict:
+            continue
+
+        with open(p, 'rb') as f:
+            i = 0
+            md5hash = hashlib.md5()
+            for i, l in enumerate(f, 1):
+                md5hash.update(l)
+            info_dict[doc_id] = (i, md5hash.hexdigest())
+    return info_dict
+
+
+def _generate_examples_from_dataset(p: Path, split: str, dataset_id: int, info_dict):
+    meta_path = p / f"{split}.csv"
+    docs_path = p / "texts"
+    if not meta_path.exists():
+        raise RuntimeError(f"Not found {split}.csv in {p}")
+    if not docs_path.exists():
+        raise RuntimeError(f"Not found texts folder in {p}")
+    with open(meta_path, 'r', encoding='utf8') as f:
+        reader = csv.reader(f)
+        try:
+            next(reader)
+        except StopIteration:
+            return
+
+        for row in reader:
+            src_id, _, tgt_id, _, label, *_ = row
+            src_id = int(src_id)
+            tgt_id = int(tgt_id)
+            label = int(label)
+            yield Example(
+                dataset_id,
+                src_id,
+                tgt_id,
+                src_len=info_dict[src_id][0],
+                tgt_len=info_dict[tgt_id][0],
+                label=label,
+                src_hash=info_dict[src_id][1],
+                tgt_hash=info_dict[tgt_id][1],
+            )
---
+++ doc_enc/training/tests/test_combine_docs_sources.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import itertools
+from pathlib import Path
+import tempfile
+
+import pytest
+
+from doc_enc.training.combine_docs_sources import combine_docs_datasets
+
+
+@pytest.fixture
+def FakeTrainingData():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+
+        tmpdirname = Path(tmpdirname)
+        for n in ("ds1", "ds2"):
+            ds = tmpdirname / n
+            ds.mkdir()
+            ds_docs = ds / "texts"
+            ds_docs.mkdir()
+
+        ds1_docs_dir = tmpdirname / "ds1" / "texts"
+        with open(ds1_docs_dir / '3.txt', 'w', encoding='utf8') as f:
+            f.write("линия1\nлиния2\nлиния3")
+        with open(ds1_docs_dir / '15.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 14)
+            f.write("line")
+        with open(ds1_docs_dir / '16.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 14)
+            f.write("line+line")
+        with open(ds1_docs_dir / '1001.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 1000)
+            f.write("line")
+        with open(ds1_docs_dir / '500.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 499)
+            f.write("line")
+        with open(ds1_docs_dir / '1501.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 1500)
+            f.write("line")
+
+        with open(tmpdirname / 'ds1' / 'train.csv', 'w', encoding='utf8') as f:
+            f.write("id1,t1,id2,t2,l\n")
+            f.write("3,_,1001,_,1\n")
+            f.write("3,_,1501,_,0\n")
+            f.write("15,_,500,_,1\n")
+            f.write("15,_,1501,_,0\n")
+            f.write("15,_,1001,_,0\n")
+            f.write("16,_,1501,_,1\n")
+
+        ds2_docs_dir = tmpdirname / "ds2" / "texts"
+        with open(ds2_docs_dir / '3.txt', 'w', encoding='utf8') as f:
+            f.write("line1\nline2\nline3")
+        with open(ds2_docs_dir / '15.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 14)
+            f.write("line")
+        with open(ds2_docs_dir / '20.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 19)
+            f.write("line")
+        with open(ds2_docs_dir / '1001.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 2000)
+            f.write("line")
+        with open(ds2_docs_dir / '600.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 599)
+            f.write("line")
+        with open(ds2_docs_dir / '400.txt', 'w', encoding='utf8') as f:
+            f.write("line\n" * 399)
+            f.write("line")
+
+        with open(tmpdirname / 'ds2' / 'train.csv', 'w', encoding='utf8') as f:
+            f.write("id1,t1,id2,t2,l\n")
+            f.write("3,_,600,_,1\n")
+            f.write("3,_,400,_,0\n")
+            f.write("15,_,1001,_,1\n")
+            f.write("20,_,1001,_,1\n")
+            f.write("20,_,600,_,0\n")
+
+        yield tmpdirname
+
+
+def _parse_combined_output(l):
+    ds, src, tgt, label, slen, tlen, shash, thash = l.rstrip().split(',')
+    return (ds, int(src), int(tgt), int(label), int(slen), int(tlen), shash, thash)
+
+
+def test_gen_basic(FakeTrainingData):
+    combine_docs_datasets(FakeTrainingData, 'train')
+    assert (FakeTrainingData / "combined_train.csv").exists()
+
+    with open(FakeTrainingData / "combined_train.csv", 'r', encoding='utf8') as f:
+        id1_3_list = [_parse_combined_output(l) for l in itertools.islice(f, 1, 5)]
+        assert len(id1_3_list) == 4
+        print(id1_3_list)
+        for t in id1_3_list:
+            assert t[1] == 3
+            assert t[4] == 3
+        for t in id1_3_list[:2]:
+            assert t[0] == 'ds1'
+        for t in id1_3_list[2:]:
+            assert t[0] == 'ds2'
+
+        assert id1_3_list[0][2] == 1001
+        assert id1_3_list[2][2] == 600
+
+        id1_15_list = [_parse_combined_output(l) for l in itertools.islice(f, 0, 5)]
+        assert len(id1_15_list) == 5
+        print(id1_15_list)
+        assert id1_15_list[0][1] == 16
+        assert id1_15_list[0][4] == 15
+        for t in id1_15_list[1:]:
+            assert t[1] == 15
+            assert t[4] == 15
+
+        src_hash = id1_15_list[1][-2]
+        assert id1_15_list[0][-2] != src_hash
+        for t in id1_15_list[1:]:
+            assert t[-2] == src_hash
