268d9141b3b748c38b7656ba01d464c6412d140f
Denis Zubarev <dvzubarev@yandex.ru>
2022-05-12 15:52:23 +0300

    bump version

--- nix/default.nix
+++ nix/default.nix
@@ -6,6 +6,7 @@
   sentencepiece,
   pytorch-bin,
   hydra,
+  boto3,
   mlflow-skinny,
   pytest,
 
@@ -13,12 +14,12 @@
 
 buildPythonPackage {
   pname = "doc_enc";
-  version = "0.0.6";
+  version = "0.0.7";
   inherit src;
 
 
   buildInputs = [];
-  propagatedBuildInputs=[scikitlearn numpy pytorch-bin sentencepiece hydra mlflow-skinny];
+  propagatedBuildInputs=[scikitlearn numpy pytorch-bin sentencepiece hydra boto3 mlflow-skinny];
   checkInputs = [pytest];
 
   checkPhase = "pytest";
